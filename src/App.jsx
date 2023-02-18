cac4c962a76ae04f29a82b3972513c5c41ea8af4
moralesvictorr <vmmorales@unicauca.edu.co>
2023-02-17 23:07:59 -0500

    Logic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,119 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Title from './components/Title'
+import TodoInput from './components/TodoInput'
 import './App.css'
+import TodoList from './components/TodoList'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Learn React',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Learn Node',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Learn Mongo',
+      completed: false,
+    },
+    {
+      id: 4,
+      title: 'Learn Express',
+      completed: false,
+    }
+  ])
+
+
+
+  const [activeFilter, setActiveFilter] = useState('all');
+
+  const [filteredTodos, setFilteredTodos] = useState(todos);
+
+  const addTodo = (title) => {
+    const lastId = todos.length > 0 ? todos[todos.length - 1].id : 1;
+
+    const newTodo = {
+      id: lastId + 1,
+      title,
+      completed: false
+    }
+
+    const todoList = [...todos]
+    todoList.push(newTodo);
+
+    setTodos(todoList);
+  }
+
+  const handleSetComplete = (id) => {
+
+    const updatedList = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed }
+      }
+      return todo;
+    })
+
+    setTodos(updatedList);
+  }
+
+  const handleClearComplete = () => {
+    const updatedList = todos.filter(todo => !todo.completed);
+    setTodos(updatedList);
+  };
+
+  const handleDelete = (id) => {
+    const updatedList = todos.filter(todo => todo.id !== id);
+    setTodos(updatedList);
+  }
+
+  const showAllTodos = () => {
+    setActiveFilter('all')
+  }
+
+  const showActiveTodos = () => {
+    setActiveFilter('active')
+  }
+
+  const showCompletedTodos = () => {
+    setActiveFilter('completed')
+  }
+
+  useEffect(() => {
+    if (activeFilter === 'all') {
+      setFilteredTodos(todos);
+    } else if (activeFilter === 'active') {
+      const activeTodos = todos.filter(todo => todo.completed === false);
+      setFilteredTodos(activeTodos);
+    } else if (activeFilter === 'completed') {
+      const completedTodos = todos.filter(todo => todo.completed === true);
+      setFilteredTodos(completedTodos);
+    }
+
+  }, [activeFilter, todos]);
 
   return (
-    <div className="App">
-      <h1 className='text-6xl text-yellow-700'>To do app</h1>
+    <div className='bg-gray-900 min-h-screen font-inter h-full text-gray-100 flex items-center justify-center py-20 px-5'>
+      <div className='container flex flex-col max-w-xl'>
+        <Title />
+        <TodoInput addTodo={addTodo} />
+        <TodoList
+          activeFilter={activeFilter}
+          todos={filteredTodos}
+          showAllTodos={showAllTodos}
+          showActiveTodos={showActiveTodos}
+          showCompletedTodos={showCompletedTodos}
+          handleSetComplete={handleSetComplete}
+          handleDelete={handleDelete}
+          handleClearComplete={handleClearComplete} />
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
