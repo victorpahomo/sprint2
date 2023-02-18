5fda0dbb71974bf68d1706a95ddec6153d1b33ed
moralesvictorr <vmmorales@unicauca.edu.co>
2023-02-17 23:08:24 -0500

    feat: New Component title

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <h1 className='text-5xl font-josefin font-bold tracking-widest'>
+        TO DO
+    </h1>
+  )
+}
+
+export default Title
