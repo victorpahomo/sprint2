ec2b92c8b610c1df9a8f14488ab0fc6adbefcea6
moralesvictorr <vmmorales@unicauca.edu.co>
2023-02-17 23:09:19 -0500

    New component List of Todos

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import TodoFilters from './TodoFilters'
+import { Todo } from './Todo'
+
+
+const TodoList = ({ todos, handleSetComplete,handleDelete,activeFilter,showAllTodos,showActiveTodos,showCompletedTodos,handleClearComplete }) => {
+  return (
+    <div className='flex flex-col mt-7 rounded-lg overflow-hidden shadow-2xl'>
+      {todos.map(todo => {
+        return (
+          <Todo 
+            key={todo.id} 
+            todo={todo} 
+            handleSetComplete={handleSetComplete}
+            handleDelete={handleDelete}
+            />
+        )
+      })}
+      <TodoFilters
+        activeFilter={activeFilter}
+        total={todos.length}
+        showAllTodos={showAllTodos}
+        showActiveTodos={showActiveTodos}
+        showCompletedTodos={showCompletedTodos}
+        handleClearComplete={handleClearComplete}
+      />
+    </div>
+  )
+}
+
+export default TodoList
